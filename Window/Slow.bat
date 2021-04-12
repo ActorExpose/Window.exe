5afb5d1296998966ac95cda7842884616a98cd06
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-04-12 16:31:36 -0600

    Fixed wallpaper bug

--- Window/Slow.bat
+++ Window/Slow.bat
@@ -1,3 +1,8 @@
 @echo off
-Set-WallPaper -value "C:\Users\%USERNAME%\Desktop\Window\pic.jpg"
-start end.vbs
+for /l %%x in (1, 1, 3) do (
+	echo %%x
+	reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %CD%\Wall.bmp /f
+	RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+	timeout 1 > null
+)
+REM start end.vbs
